ff976dfeb63ad14167e08432e7cffdb771f5aa78
rajatmoeving <127092550+rajatmoeving@users.noreply.github.com>
2024-03-22 09:40:50 +0530

    update the contect and login page

--- src/components/holipage.jsx
+++ src/components/holipage.jsx
@@ -20,10 +20,10 @@ const Holipage = () => {
                 <a className="btn btn-light" href="#">Home <span className="sr-only">(current)</span></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Contact</a>
+                <a className="nav-link" href="#"></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">About</a>
+                <a className="nav-link" href="#"></a>
               </li>
 
               <li className="nav-item dropdown">
